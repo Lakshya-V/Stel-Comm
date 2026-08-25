3c14d77b67513b2fd0a10105b81d07ce643bdccd
unknown <lakshyavats11@gmail.com>
2026-08-25 11:22:32 +0530

    Hand Recog. Added

--- detect.py
+++ detect.py
@@ -1,16 +1,58 @@
 import cv2
+import time
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+model_path = 'hand_landmarker.task'
+
+base_options = python.BaseOptions(model_asset_path=model_path)
+options = vision.HandLandmarkerOptions(
+    base_options=base_options,
+    running_mode=vision.RunningMode.VIDEO,
+    num_hands=1
+)
+
+landmarker = vision.HandLandmarker.create_from_options(options)
+
+mp_hands = mp.tasks.vision.HandLandmarksConnections
+mp_drawing = mp.tasks.vision.drawing_utils
+mp_drawing_styles = mp.tasks.vision.drawing_styles
 
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened() :
     ret, frame = cap.read()
-    flipped_frame = cv2.flip(frame, 1)
+    frame = cv2.flip(frame, 1)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+    frame_timestamp = int(time.time()*1000)
+
+    results = landmarker.detect_for_video(mp_image, frame_timestamp)
+
+    if results.hand_landmarks :
+        for hand_idx, hand_landmarks in enumerate(results.hand_landmarks) :
+            mp_drawing.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style()
+            )
+
+            for joint_idx, landmark in enumerate(hand_landmarks) :
+                h, w, _ = frame.shape
+                cx, cy = int(landmark.x * w), int(landmark.y * h)
+                cv2.putText(frame, str(joint_idx), (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+
     if not ret :
         break
 
-    cv2.imshow('frame', flipped_frame)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q') :
         break
 
+landmarker.close()
 cap.release()
 cv2.destroyAllWindows()
